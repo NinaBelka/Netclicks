722f15285fd7792416c698ce815c3d67d092e31c
Нина Волчек <mysha76@mail.ru>
2020-05-25 23:57:05 +0300

    Реализовано открытие и закрытие меню

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
+  const leftMenu = document.querySelector('.left-menu'),
+    hamburger = document.querySelector('.hamburger');
+
+  // Открытие-закрытие меню
+  hamburger.addEventListener('click', () => {
+    leftMenu.classList.toggle('openMenu');
+    hamburger.classList.toggle('open');
+
+  });
+
+  // Закрытие меню при клике вне меню
+  document.addEventListener('click', (event) => {
+    const target = event.target;
+    if (!target.closest('.left-menu')) {
+      leftMenu.classList.remove('openMenu');
+      hamburger.classList.remove('open');
+    }
+  });
+
+  // Открытие вложенного меню
+  leftMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const dropdown = target.closest('.dropdown');
+    if (dropdown) {
+      dropdown.classList.toggle('active');
+      leftMenu.classList.add('openMenu');
+      hamburger.classList.add('open');
+    }
+  });
 
 
 
 
 
-
-
-
-
-  
 });
